378f2fb556efcfec099f78581ef3fcdf32816c91
Muluken Mengistu <butim016@icloud.com>
2020-02-04 14:51:18 +0300

    share links added

--- app/src/main/java/com/gcme/everybarataa/fikirinawesibFragment/FragmentTwenty5.java
+++ app/src/main/java/com/gcme/everybarataa/fikirinawesibFragment/FragmentTwenty5.java
@@ -1,13 +1,17 @@
 package com.gcme.everybarataa.fikirinawesibFragment;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.gcme.everybarataa.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * Created by buty on 12/29/15.
@@ -18,6 +22,24 @@ public class FragmentTwenty5 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragemnt_fikir_twenty5, container, false);
+
+        TextView t = view.findViewById(R.id.tvTwenty5);
+
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/TimesNewRoman.ttf");
+        t.setTypeface(tf);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab_twenty5);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String shareBody = getResources().getString(R.string.ewunetegna_yesetoch_mebt_akibari_link);
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.ewunetegna_yesetoch_mebt_akibari));
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "choose one"));
+            }
+        });
         return view;
     }
 }
